2ac2d80e9bd01a406ece9a0bd437a9defd22b340
Evan Leis <evan.explodes@gmail.com>
2015-02-15 04:27:36 -0700

    [initial] McCulloch-Pitts neuron, network, tests

---
+++ neural/mcculloch/pitts/model.py
@@ -0,0 +1,168 @@
+# McCulloch-Pitts neuron model
+#
+# Evan Leis, 2015
+#
+# References:
+#
+# [1] Editor Michael A. Arbib "The Handbook of Brain Theory and Neural Networks"
+# Cambridge, Massachusetts; London, England: MIT, 2003
+
+# The example modelling is to imitate a wired connection between inputs,
+# neurons, and outputs.
+
+
+class Input:
+    """
+    Input gate/wire.
+    """
+
+    def __init__(self, name=None, state=0):
+        self.name = name
+        self.state = state
+
+    def __repr__(self):
+        return u"Input(name=%r, state=%r)" % (self.name, self.state)
+
+
+class Neuron(object):
+    """
+    McCulloch-Pitts Neuron
+
+    [1] pp.8
+    """
+
+    def __init__(self, inputs, weights, threshold, state=0):
+        """
+        Create a neuron with the given input wires, weights for each respective
+        wire, neuron threshold, and initial output state.
+        """
+        self.inputs = inputs
+        self.weights = weights
+        self.threshold = threshold
+        self.state = state
+
+    def update(self):
+        """
+        Update the output state based on the state of the input
+
+        y(t+1) = 1 iff sum(wi * xi(t)) >= threshold
+        """
+        value = 0
+        for index in xrange(len(self.inputs)):
+            value += self.inputs[index].state * self.weights[index]
+        self.state = 1 if value >= self.threshold else 0
+
+    def __repr__(self):
+        return u"Neuron(%r, %r, %r, state=%r)" % (
+            self.inputs, self.weights, self.threshold, self.state)
+
+
+class AndNeuron(Neuron):
+    """
+    AND
+    a * 1 + b * 1 >= 2
+    """
+
+    def __init__(self, a, b, state=0):
+        super(AndNeuron, self).__init__((a, b), (1, 1), 2, state=state)
+
+
+class OrNeuron(Neuron):
+    """
+    OR
+    a * 1 + b * 1 >= 1
+    """
+
+    def __init__(self, a, b, state=0):
+        super(OrNeuron, self).__init__((a, b), (1, 1), 1, state=state)
+
+
+class NotNeuron(Neuron):
+    """
+    NOT
+    a * -1 >= -1
+    """
+
+    def __init__(self, a, state=0):
+        super(NotNeuron, self).__init__((a,), (-1,), 0, state=state)
+
+
+class NandNeuron(Neuron):
+    """
+    NAND
+    a * -1 + b * -1 >= -1
+    """
+
+    def __init__(self, a, b, state=0):
+        super(NandNeuron, self).__init__((a, b), (-1, -1), -1, state=state)
+
+
+class Network:
+    """
+    Helper class for creating and updating functioning networks.
+
+    Inputs must be named.
+    `update` returns the states of all terminating nodes as they are given during
+    construction.
+
+    Updates in a TOP-DOWN LEFT-RIGHT direction.
+
+    Avoids problems associated with cyclical networks.
+
+    A NOT connected to itself will update in alternating states forever.
+    Cool, huh?
+
+    `net["a"]` retrieves an input named "a"
+    `net[0]` retrieves the state of the first output
+    `list(net)` returns each node in the order it is evaluated (Input/Neuron)
+    `net.update(**inputs)` updates inputs by name and returns the result
+    """
+
+    def __init__(self, *outputs):
+        self.inputs = {}
+        self.outputs = outputs
+        # collect named inputs for reference by name
+        self.size = 0
+        for item in self:
+            self.size += 1
+            if isinstance(item, Input):
+                if item.name is None:
+                    raise ValueError("Inputs must have names for reference")
+                if item.name in self.inputs:
+                    raise ValueError("Duplicate input name: %r" % item.name)
+                self.inputs[item.name] = item
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.outputs[key]
+        else:
+            return self.inputs[key]
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        for item in self._iter_helper(self.outputs, []):
+            yield item
+
+    def _iter_helper(self, outputs, checked):
+        for item in outputs:
+            if item not in checked:
+                checked.append(item)
+                if isinstance(item, Neuron):
+                    for input in self._iter_helper(item.inputs, checked):
+                        yield input
+                yield item
+
+    def update(self, **inputs):
+        for name, state in inputs.iteritems():
+            self.inputs[name].state = state
+        for gate in self:
+            if isinstance(gate, Neuron):
+                gate.update()
+        return tuple(output.state for output in self.outputs)
+
+    def __repr__(self):
+        return u"Network(*%r)" % (self.outputs,)
+
+
---
+++ test/neural/mcculloch/pitts/test.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+
+# McCulloch-Pitts neuron model
+#
+# Evan Leis, 2015
+#
+# References:
+#
+# Editor Michael A. Arbib "The Handbook of Brain Theory and Neural Networks"
+# Cambridge, Massachusetts; London, England: MIT, 2003
+
+# Make sure things behave like expected
+
+from __future__ import absolute_import
+
+import unittest
+
+from neural.mcculloch.pitts import model
+
+
+class ModelTests(unittest.TestCase):
+    def test_or_neuron(self):
+        a = model.Input("")
+        b = model.Input("")
+        gate = model.OrNeuron(a, b)
+
+        a.state = 0
+        b.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 0)
+
+        a.state = 1
+        b.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+        a.state = 0
+        b.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+        a.state = 1
+        b.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+    def test_and_neuron(self):
+        a = model.Input("")
+        b = model.Input("")
+        gate = model.AndNeuron(a, b)
+
+        a.state = 0
+        b.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 0)
+
+        a.state = 1
+        b.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 0)
+
+        a.state = 0
+        b.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 0)
+
+        a.state = 1
+        b.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+    def test_not_neuron(self):
+        a = model.Input("")
+        gate = model.NotNeuron(a)
+
+        a.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+        a.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 0)
+
+    def test_nand_neuron(self):
+        a = model.Input("")
+        b = model.Input("")
+        gate = model.NandNeuron(a, b)
+
+        a.state = 0
+        b.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+        a.state = 1
+        b.state = 0
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+        a.state = 0
+        b.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 1)
+
+        a.state = 1
+        b.state = 1
+        gate.update()
+        self.assertEquals(gate.state, 0)
+
+
+    def test_simple_net(self):
+        """
+        a -\
+            AND \
+        b -/     \
+                  AND ->
+        c -\     /
+            AND /
+        d -/
+        """
+        a = model.Input("")
+        b = model.Input("")
+        g1 = model.AndNeuron(a, b)
+
+        c = model.Input("")
+        d = model.Input("")
+        g2 = model.AndNeuron(c, d)
+
+        final = model.AndNeuron(g1, g2)
+
+
+        def try_states(_a, _b, _c, _d, expected):
+            a.state = _a
+            b.state = _b
+            g1.update()
+            c.state = _c
+            d.state = _d
+            g2.update()
+            final.update()
+            self.assertEquals(final.state, expected,
+                              "%s should result in %s" %
+                              ((_a, _b, _c, _d), expected))
+
+        try_states(0, 0, 0, 0, 0)
+        try_states(0, 0, 0, 1, 0)
+        try_states(0, 0, 1, 0, 0)
+        try_states(0, 0, 1, 1, 0)
+        try_states(0, 1, 0, 0, 0)
+        try_states(0, 1, 0, 1, 0)
+        try_states(0, 1, 1, 0, 0)
+        try_states(0, 1, 1, 1, 0)
+        try_states(1, 0, 0, 0, 0)
+        try_states(1, 0, 0, 1, 0)
+        try_states(1, 0, 1, 0, 0)
+        try_states(1, 0, 1, 1, 0)
+        try_states(1, 1, 0, 0, 0)
+        try_states(1, 1, 0, 1, 0)
+        try_states(1, 1, 1, 0, 0)
+        try_states(1, 1, 1, 1, 1)
+
+
+class NetworkTests(unittest.TestCase):
+    def test_gather_inputs_collects_inputs(self):
+        a = model.Input("a")
+        b = model.Input("b")
+        c = model.Input("c")
+        d = model.Input("d")
+        e = model.Input("e")
+        f = model.Input("f")
+        g = model.Input("g")
+        h = model.Input("h")
+        net = model.Network(
+            model.AndNeuron(
+                model.AndNeuron(a, b),
+                model.AndNeuron(c, d)
+            ),
+            model.AndNeuron(
+                model.AndNeuron(e, f),
+                model.AndNeuron(g, h)
+            )
+        )
+        self.assertEquals(len(net.inputs), 8)
+        self.assertEquals(net.inputs["a"], a)
+        self.assertEquals(net.inputs["b"], b)
+        self.assertEquals(net.inputs["c"], c)
+        self.assertEquals(net.inputs["d"], d)
+        self.assertEquals(net.inputs["e"], e)
+        self.assertEquals(net.inputs["f"], f)
+        self.assertEquals(net.inputs["g"], g)
+        self.assertEquals(net.inputs["h"], h)
+
+    def test_getitem(self):
+        a = model.Input("a")
+        b = model.Input("b")
+        c = model.Input("c")
+        d = model.Input("d")
+        e = model.Input("e")
+        f = model.Input("f")
+        g = model.Input("g")
+        h = model.Input("h")
+        andAA = model.AndNeuron(a, b)
+        andAB = model.AndNeuron(c, d)
+        andA = model.AndNeuron(andAA, andAB)
+        andBA = model.AndNeuron(e, f)
+        andBB = model.AndNeuron(g, h)
+        andB = model.AndNeuron(andBA, andBB)
+        net = model.Network(andA, andB)
+        self.assertEquals(net["a"], a)
+        self.assertEquals(net["b"], b)
+        self.assertEquals(net["c"], c)
+        self.assertEquals(net["d"], d)
+        self.assertEquals(net["e"], e)
+        self.assertEquals(net["f"], f)
+        self.assertEquals(net["g"], g)
+        self.assertEquals(net["h"], h)
+        self.assertEquals(net[0], andA)
+        self.assertEquals(net[1], andB)
+
+    def test_iter_order(self):
+        a = model.Input("a")
+        b = model.Input("b")
+        c = model.Input("c")
+        d = model.Input("d")
+        e = model.Input("e")
+        f = model.Input("f")
+        g = model.Input("g")
+        h = model.Input("h")
+        andAA = model.AndNeuron(a, b)
+        andAB = model.AndNeuron(c, d)
+        andA = model.AndNeuron(andAA, andAB)
+        andBA = model.AndNeuron(e, f)
+        andBB = model.AndNeuron(g, h)
+        andB = model.AndNeuron(andBA, andBB)
+        net = model.Network(andA, andB)
+
+        self.assertListEqual(list(net),
+                             [a, b, andAA, c, d, andAB, andA, e, f, andBA, g, h,
+                              andBB, andB])
+
+
+    def test_update(self):
+        a = model.Input("a")
+        b = model.Input("b")
+        c = model.Input("c")
+        d = model.Input("d")
+        e = model.Input("e")
+        f = model.Input("f")
+        g = model.Input("g")
+        h = model.Input("h")
+        net = model.Network(
+            model.AndNeuron(
+                model.AndNeuron(a, b),
+                model.AndNeuron(c, d)
+            ),
+            model.AndNeuron(
+                model.AndNeuron(e, f),
+                model.AndNeuron(g, h)
+            )
+        )
+
+        states = net.update(a=1, b=0, c=1, d=0, e=1, f=0, g=1, h=0)
+        self.assertEquals(net["a"].state, 1)
+        self.assertEquals(net["b"].state, 0)
+        self.assertEquals(net["c"].state, 1)
+        self.assertEquals(net["d"].state, 0)
+        self.assertEquals(net["e"].state, 1)
+        self.assertEquals(net["f"].state, 0)
+        self.assertEquals(net["g"].state, 1)
+        self.assertEquals(net["h"].state, 0)
+        self.assertEquals(states, (0, 0))
+        states = net.update(a=0, b=1, c=0, d=1, e=0, f=1, g=0, h=1)
+        self.assertEquals(net["a"].state, 0)
+        self.assertEquals(net["b"].state, 1)
+        self.assertEquals(net["c"].state, 0)
+        self.assertEquals(net["d"].state, 1)
+        self.assertEquals(net["e"].state, 0)
+        self.assertEquals(net["f"].state, 1)
+        self.assertEquals(net["g"].state, 0)
+        self.assertEquals(net["h"].state, 1)
+        self.assertEquals(states, (0, 0))
+        states = net.update(a=1, b=1, c=1, d=1, e=0, f=0, g=0, h=0)
+        self.assertEquals(net["a"].state, 1)
+        self.assertEquals(net["b"].state, 1)
+        self.assertEquals(net["c"].state, 1)
+        self.assertEquals(net["d"].state, 1)
+        self.assertEquals(net["e"].state, 0)
+        self.assertEquals(net["f"].state, 0)
+        self.assertEquals(net["g"].state, 0)
+        self.assertEquals(net["h"].state, 0)
+        self.assertEquals(states, (1, 0))
+        states = net.update(a=0, b=0, c=0, d=0, e=1, f=1, g=1, h=1)
+        self.assertEquals(net["a"].state, 0)
+        self.assertEquals(net["b"].state, 0)
+        self.assertEquals(net["c"].state, 0)
+        self.assertEquals(net["d"].state, 0)
+        self.assertEquals(net["e"].state, 1)
+        self.assertEquals(net["f"].state, 1)
+        self.assertEquals(net["g"].state, 1)
+        self.assertEquals(net["h"].state, 1)
+        self.assertEquals(states, (0, 1))
+
+    def test_can_create_cyclical_network(self):
+
+        a = model.NotNeuron(None)
+        a.inputs = [a]
+
+        net = model.Network(a)
+
+        self.assertEqual(list(net), [a])
+
+    def test_cyclical_network_doesnt_explode_during_update(self):
+        a = model.NotNeuron(None)
+        a.inputs = [a]
+        net = model.Network(a)
+        net.update()
+
+    def test_cyclical_network_is_self_propelling(self):
+        a = model.NotNeuron(None, state=0)
+        a.inputs = [a]
+        net = model.Network(a)
+        self.assertEqual(a.state, 0)
+        net.update()
+        self.assertEqual(a.state, 1)
+        net.update()
+        self.assertEqual(a.state, 0)
+        net.update()
+        self.assertEqual(a.state, 1)
+        net.update()
+        self.assertEqual(a.state, 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
